a94fde631b83a98d628e0dc9432dfb5db7a56f1c
Nathan Hollows <me@nathanhollows.com>
2021-07-03 14:48:15 +1200

    Redirect logged in players

--- pkg/handler/public/index.go
+++ pkg/handler/public/index.go
@@ -5,18 +5,28 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/nathanhollows/AmazingTrace/pkg/flash"
 	"github.com/nathanhollows/AmazingTrace/pkg/handler"
+	"github.com/nathanhollows/AmazingTrace/pkg/models"
 )
 
 // Index is the homepage of the game.
 // Prints a very simple page asking only for a team code.
 func Index(env *handler.Env, w http.ResponseWriter, r *http.Request) error {
 	w.Header().Set("Content-Type", "text/html")
+	session, _ := env.Session.Get(r, "trace")
 	data := make(map[string]interface{})
 
-	data["messages"] = flash.Get(w, r)
+	// Check if team exists
+	teamCode := session.Values["code"]
+	result := env.DB.Where("Code = ?", teamCode).Find(&models.Team{})
+	if result.RowsAffected == 1 {
+		http.Redirect(w, r, "/clues", 302)
+	}
 	data["title"] = "The Amazing Trace"
+
+	// Get flashed messages
+	data["messages"] = session.Flashes()
+	session.Save(r, w)
 
 	templates := template.Must(template.ParseFiles(
 		"../web/templates/index.html",
